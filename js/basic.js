e68033b70013989ebe6f85e167243102d5002075
threedalpeng <threedalpeng@gmail.com>
2021-10-19 14:30:31 +0000

    initial commit

---
+++ js/basic.js
@@ -0,0 +1,12 @@
+import utils, { $id } from "./utils.js";
+
+function init() {
+  let cvs = $id("webgl-canvas");
+  let gl = cvs.getContext("webgl2");
+  let text_log = $id("cvs-log");
+  console.log(gl);
+  text_log.innerText = gl;
+  utils.attachLog();
+}
+
+window.onload = init;
---
+++ js/utils.js
@@ -0,0 +1,23 @@
+export const $q = (q) => document.querySelector(q);
+export const $qa = (q) => document.querySelectorAll(q);
+export const $id = (id) => document.getElementById(id);
+export const $cls = (cls) => document.getElementsByClassName(cls);
+
+const utils = {
+  attachLog: () => {
+    let body = $q("body");
+    let logbar = document.createElement("div");
+    logbar.setAttribute("id", "log-sidebar");
+    let text_log = document.createElement("p");
+    text_log.setAttribute("id", "log-text");
+    logbar.appendChild(text_log);
+    body.appendChild(logbar);
+    text_log.innerText = "Here is a log";
+  },
+  log: (message) => {
+    let text_log = $id("log-text");
+    text_log.innerText += message;
+  },
+};
+
+export default utils;
