991bf129bbb11bde5824e0b749ce2023cfea84d2
Kim Jeong-won <threedalpeng@gmail.com>
2021-10-20 20:10:11 +0900

    resize canvas size

--- js/basic.js
+++ js/basic.js
@@ -1,13 +1,10 @@
 import utils, { $id } from "./utils.js";
 
 function init() {
+  utils.attachLog();
+
   let cvs = $id("webgl-canvas");
   let gl = cvs.getContext("webgl2");
-  let text_log = $id("cvs-log");
-  console.log(gl);
-  text_log.innerText = gl;
-  utils.attachLog();
-  utils.log("hihi");
 }
 
 window.onload = init;
--- js/utils.js
+++ js/utils.js
@@ -6,12 +6,16 @@ export const $cls = (cls) => document.getElementsByClassName(cls);
 const utils = {
   attachLog: () => {
     let body = $q("body");
+
     let logbar = document.createElement("div");
     logbar.setAttribute("id", "log-sidebar");
+
     let text_log = document.createElement("p");
     text_log.setAttribute("id", "log-text");
+
     logbar.appendChild(text_log);
     body.appendChild(logbar);
+
     text_log.innerText = "Here is a log";
   },
   log: (message) => {
